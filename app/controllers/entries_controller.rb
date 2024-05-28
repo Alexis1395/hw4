71c1ab68658082277b53a2c1678f71232ac97708
Alexis Pham <alexis.pham@kellogg.northwestern.edu>
2024-05-28 03:30:22 +0000

    Commit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,7 +6,7 @@ class EntriesController < ApplicationController
 
   def create
     @user = User.find_by({ "id" => session["user_id"] })
-    if @user = nil 
+    if @user == nil 
       flash["notice"] = "You need to be logged in first."
     else 
       @entry = Entry.new
@@ -14,6 +14,7 @@ class EntriesController < ApplicationController
       @entry["description"] = params["description"]
       @entry["occurred_on"] = params["occurred_on"]
       @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
       @entry.save
       redirect_to "/places/#{@entry["place_id"]}"
     end 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,31 +1,40 @@
 class PlacesController < ApplicationController
+  before_action :current_user
+
+  def current_user
+    @current_user = User.find_by({ "id" => session["user_id"] })
+  end 
 
   def index
+    if @current_user
     @places = Place.all
+    else 
+      flash["notice"] = "You need to be logged in first."
+      redirect_to "/login"
+    end
   end
 
   def show
-    @user = User.find_by({ "id" => session["user_id"] })
-    if @user = nil 
-      flash["notice"] = "You need to be logged in first."
-    else 
+    if @current_user != nil
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
+    else 
+      flash["notice"] = "You need to be logged in first."
+      redirect_to "/login"
     end 
   end
 
   def new
-    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-    @user = User.find_by({ "id" => session["user_id"] })
-    if @user = nil 
-      flash["notice"] = "You need to be logged in first."
-    else 
+    if @current_user != nil
     @place = Place.new
     @place["name"] = params["name"]
+    # need to assign user to a newly created place
     @place.save
+    else 
+    flash["notice"] = "You need to be logged in first."
     end
     redirect_to "/places"
   end
